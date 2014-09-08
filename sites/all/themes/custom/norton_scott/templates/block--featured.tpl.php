567e6587e4816c04f128eba329174a0f66e70edb
Jennifer Mattson <jennifer@Jennifermattson.com>
2014-09-08 06:35:35 -0700

    download to icon + mobile display for video/logo area

--- sites/all/themes/custom/norton_scott/templates/block--featured.tpl.php
+++ sites/all/themes/custom/norton_scott/templates/block--featured.tpl.php
@@ -45,7 +45,7 @@
  */
 ?>
   
-  <div class="small-7 small-centered columns">
+  <div class="medium-7 small-12 small-centered columns">
     <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
     <?php print render($title_prefix); ?>
